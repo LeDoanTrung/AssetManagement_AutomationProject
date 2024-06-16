dd0e51b1e836231220bd17d3a5efa38ee2e8474d
LeDoanTrung <86414540+LeDoanTrung@users.noreply.github.com>
2024-06-16 21:41:50 +0700

    Update Element class

--- Library/Element.cs
+++ Library/Element.cs
@@ -1,10 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AssetManagement.Library
 {
