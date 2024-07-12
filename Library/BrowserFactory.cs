cd8d7776e352bc2be0b222348b91a09c4ed95cce
tiennguyena1 <tiennguyena1@nashtechglobal.com>
2024-07-12 09:05:29 +0000

    Updated BrowserFactory.cs

--- Library/BrowserFactory.cs
+++ Library/BrowserFactory.cs
@@ -20,9 +20,10 @@ namespace AssetManagement.Library
             {
                 case "chrome":
                     var chromeOptions = new ChromeOptions();
-                    chromeOptions.AddArguments("test-type");
-                    chromeOptions.AddArguments("--no-sandbox");
-                    WebDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), chromeOptions);
+                    //chromeOptions.AddArguments("test-type");
+                    //chromeOptions.AddArguments("--no-sandbox");
+                    //WebDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), chromeOptions);
+                    WebDriver =  new ChromeDriver();
                     break;
 
                 case "edge":
